c118d7cd12bc70079b7c9edb8e6bd039f638d376
Oleg_Kolomin <89177999655i@gmail.com>
2023-02-26 00:22:46 +0500

    две задачи

---
+++ zadacha 2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Write("Ведите первое число: ");
+int namber1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите второе число: ");
+int namber2 = Convert.ToInt32(Console.ReadLine());
+
+if ( namber1 < namber2 ) 
+{
+    Console.Write($"Min {namber1} Max {namber2}");
+}
+else
+{
+    Console.Write ($"Max {namber1} Min {namber2}");
+}
+
---
+++ zadacha 4/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Ведите первое число: ");
+int namber1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Ведите второе число: ");
+int namber2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Ведите третее число: ");
+int namber3 = Convert.ToInt32(Console.ReadLine());
+
+int Max = namber1;
+
+if (namber1 > Max)
+{
+    Max = namber1;
+}
+if (namber2 > Max)
+{
+    Max = namber2;
+}
+if (namber3 > Max)
+{
+    Max = namber3;
+}
+Console.Write ($"Max {Max}");
