c1be5295af253a6446f0cb436cfc989536335584
Oleg_Kolomin <89177999655i@gmail.com>
2023-02-28 15:52:30 +0500

    + 8 zadacha

---
+++ zadacha 8/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+
+
+//Задача 8: Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+
+
+Console.Write("Ведите первое число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int temp = 1;
+
+
+while (temp <= N)
+ {
+     if (temp%2 == 0)
+          {
+               Console.Write($"{temp} ");
+          }
+
+     temp++;
+}
+
+
